2416398602876d3c5b2ba03c1c8630b1191c432a
Rohankorale <87647567+Rohankorale@users.noreply.github.com>
2021-07-19 18:23:58 +0530

    Update grid.py

--- grid.py
+++ grid.py
@@ -13,12 +13,14 @@ class Grid :
         self.offset = offset
 
 
+    # Creating the 2D Grid
     def random_2d_array(self):
         for x in range(self.rows) :
             for y in range(self.columns) :
                 self.grid_array[x][y] = random.randint(0,1)
 
 
+    # Setting up the rules and calculating the next generation
     def Conway(self,on_color, off_color, surface):
         for x in range(self.rows) :
             for y in range(self.columns) :
@@ -38,7 +40,7 @@ class Grid :
                 if state == 0 and neighbours == 3 :  # coming alive by reproduction
                     next[x][y] = 1
 
-                elif state == 1 and (neighbours < 2 or neighbours > 3) :  # dying of less or over population
+                elif state == 1 and (neighbours < 2 or neighbours > 3) :  # dying off due to less or over population
                     next[x][y] = 0
 
                 else :
@@ -47,14 +49,17 @@ class Grid :
         self.grid_array = next
 
 
+    # Mouse Operations
     def Handle_mouse(self,x,y):
         _x = int(x // self.scale)
         _y = int(y // self.scale)
 
+        # If condition to ensure that the mouse position is not lying out of the game-screen
         if self.grid_array[_x][_y] != None :
             self.grid_array[_x][_y] = 1
 
 
+    # Calculating total alive neighbours at a given (x,y)
     def get_neighbours(self,x,y):
         total = 0
 
